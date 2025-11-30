dd22085e0b6dad237e5b0276ddf63d596ae74b89
Ohari <d3v0h4r1@gmail.com>
2025-11-30 14:43:58 +0900

    implement handle_mouse

--- run.py
+++ run.py
@@ -10,7 +10,6 @@ The logic lives in components.Board; this module should not implement rules.
 """
 
 import sys
-
 import pygame
 
 import config
@@ -117,32 +116,36 @@ class InputController:
 
     def handle_mouse(self, pos, button) -> None:
         # TODO: Handle mouse button events: left=reveal, right=flag, middle=neighbor highlight  in here
-        # col, row = self.pos_to_grid(pos[0], pos[1])
-        # if col == -1:
-        #     return
-        # game = self.game
-        # if button == config.mouse_left:
-        #     game.highlight_targets.clear()
+        col, row = self.pos_to_grid(pos[0], pos[1])
+        if col == -1:
+            return
         
-        #         if not game.started:
-        #             game.started = 
-        #             game.start_ticks_ms = pygame.time.get_ticks()
+        game = self.game
+        
+        if button == config.mouse_left:
+            game.highlight_targets.clear()
+        
+            if not game.started:
+                game.started = True
+                game.start_ticks_ms = pygame.time.get_ticks()
+
+            game.board.reveal(col, row)
     
-        # elif button == config.mouse_right:
-        #     game.highlight_targets.clear()
-        #        
-        # elif button == config.mouse_middle:
-        #         neighbors = []
-        #         game.highlight_targets = {
-        #             (nc, nr)
-        #             for (nc, nr) in neighbors
-        #             if not game.board.cells[game.board.index(nc, nr)].state.is_revealed
-        #         }
+        elif button == config.mouse_right:
+            game.highlight_targets.clear()
+            game.board.toggle_flag(col, row)
+
+        elif button == config.mouse_middle:
+                neighbors = game.board.neighbors(col, row)
+
+                game.highlight_targets = {
+                    (nc, nr)
+                    for (nc, nr) in neighbors
+                    if not game.board.cells[game.board.index(nc, nr)].state.is_revealed
+                }
         
-        #         game.highlight_until_ms = pygame.time.get_ticks() + config.highlight_duration_ms
-
-        pass
-
+                game.highlight_until_ms = pygame.time.get_ticks() + config.highlight_duration_ms
+        
 class Game:
     """Main application object orchestrating loop and high-level state."""
 
